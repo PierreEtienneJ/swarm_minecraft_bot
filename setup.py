46964abfffc79039722fd1e9096489bb74ba924b
PierreEtienneJ <pierreetiennejosse@hotmail.com>
2023-02-23 20:54:02 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+  
+setup(
+    name='swarm_minecraft_bot',
+    version='0.1',
+    description='A Python package to make a swarm minecraft bots',
+    author='Pierre-Etienne',
+    install_requires=[
+        'javascript',
+    ],
+)
